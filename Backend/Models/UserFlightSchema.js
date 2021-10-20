e54b7cc14df8ebc3c81f3063b15676bc4a2ca66c
surbhi <surbhi.dogra@sjsu.edu>
2021-10-20 14:08:33 -0700

    Adding userflightschema for flight details

---
+++ Backend/Models/UserFlightSchema.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+
+
+const userFlightSchema = new mongoose.Schema(
+{
+        userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"UserSchema",
+        },
+        flightId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"FlightSchema"
+        },
+        class:{
+        type: String,
+        required: true,
+
+        },
+        
+        numberOfPassengers:{
+            type:Number,
+            required:true
+        },
+
+        ticketNumber:{
+            type: mongoose.Schema.Types.ObjectId,
+            required:true
+        },
+
+        seatNumber:{
+            type:String,
+            required:true,
+            maxlength:3
+
+        }
+
+}
+)
