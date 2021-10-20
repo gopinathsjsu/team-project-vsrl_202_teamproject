b1d139a9bbbc3cc6a3b9efa54ca39fd36d217012
nikithakallepalli <kallepallinikitha@gmail.com>
2021-10-20 12:26:21 -0700

    created userschema

---
+++ Backend/Models/userSchema.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+const userSchema = new mongoose.Schema(
+{
+        firstName: {
+        type: String,
+        required: true,
+        maxlength: 32,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        maxlength: 32,
+        true: true
+    },
+    Address: {
+        type: String,
+        maxlength: 150,
+    },
+    EmailId: {
+        type: String,
+        maxlength: 50,
+        unique: true,
+        required: true
+    },
+    PhoneNumber: {
+        type: Number,
+        maxlength: 10,
+        unique: true,
+        required: true
+    },
+
+
+}
+)
