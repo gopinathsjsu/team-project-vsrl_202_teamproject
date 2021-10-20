32c0c12c6ad14d1aa080fe4c266d8b99acdeb38e
Vineet Karmiani <vineetkarmiani@gmail.com>
2021-10-20 13:53:56 -0700

    Added role and password in user schema

--- Backend/Models/userSchema.js
+++ Backend/Models/userSchema.js
@@ -29,7 +29,48 @@ const userSchema = new mongoose.Schema(
         unique: true,
         required: true
     },
+      encry_password: {
+        type: String,
+        required: true
+      },
+    role: {
+        type: Number,
+        default: 0
+      },
+    rewardPoints: {
+        type: Number,
+        default: 0
+    }
+ }
 
-
-}
 )
+
+
+userSchema.virtual('password').
+set(function(password){
+  this._password=password;
+  this.salt = uuidv1();
+  this.encry_password = this.securePassword(password)
+})
+.get(function(){
+  return this._password;
+})
+userSchema.methods = {
+securePassword: function(plainpassword){
+  if(plainpassword=="")
+    return "";
+  try {
+    const secret = this.salt;
+    return crypto.createHmac('sha256', secret)
+                 .update(plainpassword)
+                 .digest('hex');
+  } catch (error) {
+    return "";
+  }
+},
+authenticate: function(plainpassword){
+    return this.securePassword(plainpassword)===this.encry_password;
+}
+}
+
+module.exports = mongoose.model("UserSchema",userSchema);
