078d917a88db0444baa28f3ae68098da56a7b2a0
nikithakallepalli <kallepallinikitha@gmail.com>
2021-10-20 16:26:18 -0700

    changes made to the user controller and route

--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const{bookFlight}= require("../controllers/userController");
+const{bookFlight, getUserById}= require("../controllers/userController");
+const{getFlightById}= require("../controllers/adminController");
 router.param("userId",getUserById);
+router.param("flightId",getFlightById);
 router.post("/user/bookFlight/:userId/:flightId",isSignedIn,isAuthenticated, bookFlight);
 module.exports = router;
