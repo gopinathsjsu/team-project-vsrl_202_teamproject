19e76dd42884a9f1f15ad116ca236d4b84218a8c
Lakshmi1411 <lakshmi.gundlapalli@sjsu.edu>
2021-10-20 16:28:34 -0700

    commit admin routes and controller methods

---
+++ Backend/Controllers/adminController.js
@@ -0,0 +1,26 @@
+const flight = require("../Models/FlightSchema");
+const admin = require("../Models/userSchemaSchema");
+exports.createFight=(req,res) =>{
+
+}
+exports.editFight=(req,res) =>{
+
+}
+exports.editFights=(req,res) =>{
+
+}
+exports.deleteFight=(req,res) =>{
+
+}
+exports.deleteFights=(req,res) =>{
+
+}
+exports.getAllFights=(req,res) =>{
+
+}
+exports.getFight=(req,res) =>{
+
+}
+exports.getFlightById =(req,res) =>{
+
+}
