fd32aad0c5a7d3c17c1ddf133a58a94b4e9eb341
Lakshmi1411 <lakshmi.gundlapalli@sjsu.edu>
2021-10-20 17:35:44 -0700

    commit updates

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,9 +1,7 @@
 const user = require("../Models/userSchema")
 exports.bookFlight = (req,res) => {}
-<<<<<<< HEAD
 exports.getCurrentFlights = (req,res) => {}
-=======
 exports.getUserById = (req,res) => {}
 
->>>>>>> 720000ad4fb8ba6f8cd616286661487a59154740
+
 
