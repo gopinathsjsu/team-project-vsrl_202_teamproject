598a5c2f6939c0c2a0fa4072049e665d16c415b2
nikithakallepalli <kallepallinikitha@gmail.com>
2021-10-27 14:42:22 -0700

    changes made to admin route and controller

--- Backend/Controllers/adminController.js
+++ Backend/Controllers/adminController.js
@@ -31,6 +31,52 @@ exports.passengerDetails =(req,res) =>{
 exports.pilotDetails =(req,res) =>{
 
 }
-exports.airhostessDetails =(req,res) =>{
-
+exports.createairhostessDetails =(req,res) =>{
+const airhostess = new userSchema(req.body)
+airhostess.role=2;
+airhostess.save()
+.then((airhostess)=>{
+    return{
+        airhostess: airhostess.firstName
+    };
+})
+.catch((err)=>{
+    return{
+        error: "Not able to create passenger"
+    };  
+})
 }
+exports.createPassengerDetails =(req,res) =>{
+const passenger = new userSchema(req.body)
+passenger.role = 3;
+passenger.save()
+.then((passenger)=>{
+    return {
+        passengername: passenger.firstName
+    };
+})
+.catch((err)=>{
+    return{
+        error: "Not able to create passenger"
+    };
+}
+
+)
+}
+
+exports.createPilotDetails =(req,res) =>{
+    const pilot = new userSchema(req.body)
+    pilot.role=1;
+    pilot.save()
+    .then((pilot)=>{
+        return{
+            pilot: pilot.firstName
+        };
+    })
+    .catch((err)=>{
+        return{
+            error: "Not able to create pilot"
+        };  
+    })
+    }
+
