49899c5775964761bb80a09af20c9193fc9fb45b
Vineet Karmiani <vineetkarmiani@gmail.com>
2021-11-09 22:11:33 -0800

    Adding home.js file

--- airline-application-frontend/src/core/Home.js
+++ airline-application-frontend/src/core/Home.js
@@ -1,1 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
+import Header from './Header'
+import "../css/Home.css"
+const Home = ()=>{
+    return(
+        <div className="home">
+            {/* <AppBar/> */}
+            <p>HI</p>
+            <Header/>
+            
+        </div>
+    )
+}
+
+export default Home;
