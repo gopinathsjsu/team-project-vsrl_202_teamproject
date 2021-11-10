49a59cc6fc3adbcf12a246ad28d2fac825bd9cc0
Vineet Karmiani <vineetkarmiani@gmail.com>
2021-11-10 02:30:53 -0800

    Adding tabs

--- airline-application-frontend/src/core/Header.js
+++ airline-application-frontend/src/core/Header.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "../css/Header.css"
-import Container from '@mui/material/Container';
+import { Tab, Tabs, Container } from 'react-bootstrap'
+import BookFlight from "../components/BookFlight";
+import MyTrips from "../components/MyTrips";
+import Checkin from "../components/Checkin";
+import FlightStatus from '../components/FlightStatus';
 
 const Header = ()=>{
+    const [key, setKey] = useState('Book');
+
     return (
         <div className="header" >
-            <p></p>
+            <Container>
+                <Tabs
+                id="controlled-tab-example"
+                activeKey={key}
+                onSelect={(k) => setKey(k)}
+                color="var(--primary1)"
+                className="mb-3"
+                >
+                    <Tab eventKey="Book" title="Book">
+                        <BookFlight/>
+                    </Tab>
+                    <Tab eventKey="MyTrips" title="My Trips">
+                        <MyTrips/>
+                    </Tab>
+                    <Tab eventKey="Check-in" title="Check-in">
+                        {/* <Sonnet /> */}
+                        <Checkin/>
+                    </Tab>
+                    <Tab eventKey="status" title="Flight status">
+                        {/* <Sonnet /> */}
+                        <FlightStatus/>
+                    </Tab>
+                </Tabs>
+            </Container>
         </div>
     )
 }
