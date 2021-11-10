ac96e0ff940336554f28e479343a4e96d40043d9
Vineet Karmiani <vineetkarmiani@gmail.com>
2021-11-09 22:12:10 -0800

    Adding header.js file

---
+++ airline-application-frontend/src/core/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../css/Header.css"
+import Container from '@mui/material/Container';
+
+const Header = ()=>{
+    return (
+        <div className="header" >
+            <p></p>
+        </div>
+    )
+}
+
+export default Header;
