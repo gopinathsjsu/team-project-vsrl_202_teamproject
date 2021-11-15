b17fc5d11bf1430c25596f727d32c3ddc643c5cf
surbhi <surbhi.dogra@sjsu.edu>
2021-11-15 00:58:37 -0800

    Toastify addition for form validation

--- airline-application-frontend/src/components/login.component.js
+++ airline-application-frontend/src/components/login.component.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
+import { ToastContainer,toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import { Form,Button,Container,Row,Col } from "react-bootstrap";
 export default class Login extends Component {
+    
+
     render() {
         return (
                 <Container>
@@ -8,7 +12,7 @@ export default class Login extends Component {
                     <Row className="mt-5">
                         <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
                         <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+                     <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
 
                     <Form.Control type="email" placeholder="Enter email" />
