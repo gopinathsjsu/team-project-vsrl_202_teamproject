842cc04b99923be2397bd450241f659cd858b405
surbhi <surbhi.dogra@sjsu.edu>
2021-11-16 14:03:04 -0800

    added functions

--- airline-application-frontend/src/components/login.component.js
+++ airline-application-frontend/src/components/login.component.js
@@ -2,10 +2,45 @@ import React, { Component,useState } from "react";
 import { ToastContainer,toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import { Form,Button,Container,Row,Col } from "react-bootstrap";
+import { signup } from "../authHelper";
+
+
+const Funcs=()=>{
+    const [values,setValues]=useState({
+        email:"",
+        password:"",
+        error:"",
+        success:false
+    });
+    
+    const {name,email,password,error,success}=values;
+    
+    const handleChange=name=>event=>{
+        setValues({...values,error:false,[name]:event.target.value})
+    }
+
+    const onSubmit = event =>{
+        event.preventDefault();
+        setValues({...values,error:false});
+         signup({email,password})
+         .then(data=>{
+             if(data.error){
+                 setValues({...values,error:data.error,success:false});
+             }
+             else{
+                 setValues({...values,email:"",password:"",success:true});
+             }
+         })
+         .catch(console.log("Error in signup "))
+    };
+    
+};
+
 export default class Login extends Component {
    
 
     render() {
+    
         return (
                 <Container>
                     <h1 className="shadow-sm text-success mt-5 p-3 text-center rounded">Login</h1>
@@ -13,15 +48,15 @@ export default class Login extends Component {
                         <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
                         <Form>
                      <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
+                    <Form.Label >Email address</Form.Label>
 
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" onChange={Funcs.handleChange("email")} placeholder="Enter email" />
                         
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" onChange={Funcs.handleChange("password")}  />
                     </Form.Group>
                     
                     <Button variant="btn btn-success btn-block" type="submit">
